635bbacac8965e30799cba3ea13d888249a31cd5
Andreu <andreu@Andreus-MacBook-Pro.local>
2018-06-24 07:24:59 -0500

    env port Bug3

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const path = require('path');
 const Express = require('express');
 
 const publicPath = path.join(__dirname, '..', 'public');
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 console.log(proccess.env.PORT );
-// var app = Express();
-//
-// app.use(Express.static(publicPath));
-//
-// app.listen( port, () => {
-//   console.log('server is up on this prot');
-// }) ;
-//
-// console.log(__dirname+ '../public');
-// console.log(publicPath);
+var app = Express();
+
+app.use(Express.static(publicPath));
+
+app.listen( port, () => {
+  console.log('server is up on this prot');
+}) ;
+
+console.log(__dirname+ '../public');
+console.log(publicPath);
